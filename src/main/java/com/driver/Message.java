d38a4359ed208eeddb53f28b1917fa055440e31a
AdityaB420 <butola.aditya10@gmail.com>
2023-01-28 19:51:36 +0530

    First Commit

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,5 +6,50 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    private User user;
+    private Group group;
+    public Message(int id,String content){
+        this.id=id;
+        this.content=content;
+        this.timestamp=new Date();
+        this.user=user;
+        this.group=group;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int messageId) {
+        this.id = id;
+    }
+
+    public String getcontent() {
+        return content;
+    }
+
+    public void setcontent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
 
 }
--- src/main/java/com/driver/User.java
+++ src/main/java/com/driver/User.java
@@ -3,4 +3,23 @@ package com.driver;
 public class User {
     private String name;
     private String mobile;
+    public User(String name,String mobile){
+        this.name=name;
+        this.mobile=mobile;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobileNumber(String mobile) {
+        this.mobile = mobile;
+    }
 }
